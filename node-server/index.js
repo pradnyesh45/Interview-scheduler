0394fa6182d881856d4d6332e38137bf8d810542
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-11-22 16:41:32 +0530

    server folder structure setup and express app running

--- node-server/index.js
+++ node-server/index.js
@@ -1,1 +1,33 @@
-let name = "Pradnyesh Aglawe";
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const port = 5000;
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", require("./routes/index"));
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// mongoose connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error to mongoDB: "));
+db.once("open", function () {
+  console.log("Connected successfully to mongoDB");
+});
+
+// express server
+app.listen(port, () => {
+  console.log(
+    `Server of Interview-scheduler listening at http://localhost:${port}`
+  );
+});
